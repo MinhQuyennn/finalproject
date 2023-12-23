3df0d66b76872f08e87d41f272d21f16837091b7
NghiaNgo2002 <nghiango229@gmail.com>
2023-12-23 23:14:21 +0700

    Database db4free

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,14 +2,10 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOSTNAME,
-  port: process.env.DB_PORT || 3307,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: "www.db4free.net",
+  user: "nghiango100",
+  password: "Admin_123",
+  database: "projectmhhdl",
 });
 
 module.exports = db;
