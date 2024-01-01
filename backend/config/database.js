2a2f09c6cd4b581b5cd0630469071366e521b0bd
Tarho <115691708+Tarho@users.noreply.github.com>
2024-01-01 14:31:50 +0700

    Forgot changing this :')

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host: "www.db4free.net",
-  user: "nghiango100",
-  password: "Admin_123",
-  database: "projectmhhdl",
+  user: "duongquocvu",
+  password: "admin123",
+  database: "projectlong",
 });
 
 module.exports = db;
